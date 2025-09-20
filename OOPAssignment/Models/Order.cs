a06f3014bc72215f565753f7fca41cb02ba2a398
Rayan Monfared85@gmail.com <rayanmonfared85@gmail.com>
2025-09-20 14:08:40 +0200

    project preparation

---
+++ OOPAssignment/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPAssignment.Models
+{
+    public class Order
+    {
+        public string CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string ProductName { get; set; }
+        public int QuantityOrdered { get; set; }
+    }
+}
---
+++ OOPAssignment/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPAssignment.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
